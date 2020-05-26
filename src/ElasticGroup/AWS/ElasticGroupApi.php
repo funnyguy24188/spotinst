410c0b75be1a6fb0e5e9c7f95c37b225ecb5855b
Triet Le <triet.le@codeforcevina.com>
2020-05-26 17:38:43 +0700

    update compute tab

--- src/ElasticGroup/AWS/ElasticGroupApi.php
+++ src/ElasticGroup/AWS/ElasticGroupApi.php
@@ -202,7 +202,8 @@ class ElasticGroupApi extends BaseApi
         "ondemand": "t3.small",
         "spot": [
             "t2.small",
-            "t3.small"                      
+            "t3.small", 
+            "t3a.small"                      
         ],
         "preferredSpot": [      
           "t3a.small"
